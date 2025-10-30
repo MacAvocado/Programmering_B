7cf6cc1cbbdd9cbdc0d6aa39240cb7eb8d77f3f6
MacAvocado <balder.thorseth@icloud.com>
2025-10-30 22:05:17 +0100

    i

--- 01_web_template/index.js
+++ 01_web_template/index.js
@@ -3,19 +3,21 @@ var currentPage = "#page5"
 
 function setup (){
     console.log("p5 setup kaldt")
-    var allMenuItems = selectAll(".sidebar a")
-    allMenuItems.map( 
-        function(item){
-            item.mousePressed(
-                function(){
-                    shiftPage(item.attribute("action"))
-                }
+    //set menu op
+    var allPages = selectAll(".page")
+    allPages.map(
+        page => {
+            var menuItem = createElement("a")
+            menuItem.html(page.attribute("id"))
+            menuItem.mousePressed(
+                () => shiftPage("#" + page.attribute("id"))
             )
+            select(".sidebar").child(menuItem)
         }
     )
 }
 
-
+//
 function shiftPage (newPage){
     select(currentPage).removeClass("show")
     select(newPage).addClass("show")
