cea3979c72d1193738ad2f0ef28aa0c318cd79a9
MacAvocado <balder.thorseth@icloud.com>
2025-10-31 08:45:02 +0100

    kopi fra 01_web

---
+++ 03_web_template/index.js
@@ -0,0 +1,26 @@
+var currentPage = "#page5"
+
+
+function setup (){
+    console.log("p5 setup kaldt")
+    //set menu op
+    var allPages = selectAll(".page")
+    allPages.map(
+        page => {
+            var menuItem = createElement("a")
+            menuItem.html(page.attribute("id"))
+            menuItem.mousePressed(
+                () => shiftPage("#" + page.attribute("id"))
+            )
+            select(".sidebar").child(menuItem)
+        }
+    )
+}
+
+//
+function shiftPage (newPage){
+    select(currentPage).removeClass("show")
+    select(newPage).addClass("show")
+    currentPage = newPage
+}
+
